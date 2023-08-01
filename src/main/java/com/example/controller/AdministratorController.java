7ae8709198f61d99df987c07b547e1cf9e5dda22
Yuya Nakamura <yuya.nakamura@rakus-partners.co.jp>
2023-08-01 17:59:42 +0900

    add insert method

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -12,9 +15,16 @@ import com.example.service.AdministratorService;
 @RequestMapping("/")
 public class AdministratorController {
     @Autowired
-    private AdministratorService AdministratorService;
+    private AdministratorService administratorService;
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert.html";
+    }
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        BeanUtils.copyProperties(form, administrator);
+        administratorService.insert(administrator);
+        return "redirect: /";
     }
 }
